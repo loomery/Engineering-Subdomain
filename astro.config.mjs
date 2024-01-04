53e7bea2569ed39ab6028d2fbe24dc3fad7a9281
Tom Holmes <thg.holmes96@gmail.com>
2024-01-04 17:49:44 +0000

    Updates Astro 4.1

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,5 +4,24 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
+  site: "https://tommy-holmes.github.io",
+  base: "/Loomery-Lab-Prod",
+  output: "static",
+  build: {
+    format: "directory",
+    // assetsPrefix: "",
+  },
+  vite: {
+    build: {
+      rollupOptions: {
+        output: {
+          assetFileNames: "assets/asset.[hash][extname]",
+        }
+      }
+    }
+  },
+  server: {
+    tailwindConfig: './tailwind.config.js',
+  },
   integrations: [tailwind()]
 });
